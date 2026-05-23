bb699dd7aee581587eb86c6becbf0973e5c1a8cb
Ashish <ashishsn3301@gmail.com>
2026-05-23 15:37:30 +0530

    updated login

--- studyplanner/src/pages/Login.jsx
+++ studyplanner/src/pages/Login.jsx
@@ -21,7 +21,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await api.post("api/login/", data);
+      const response = await api.post("/api/login/", data);
 
       localStorage.setItem("token", response.data.access);
       localStorage.setItem("refresh",response.data.refresh);

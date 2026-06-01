4911870f5fd10234620fe42437897e4bba5d7c5f
Ashish <ashishsn3301@gmail.com>
2026-06-01 20:41:23 +0530

    cloudinary updated in settings

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -167,5 +167,7 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': env('CLOUDINARY_API_SECRET'),
 }
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 # Tell Django to use Cloudinary for any uploaded media/documents
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
